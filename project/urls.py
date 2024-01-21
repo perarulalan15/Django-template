314218c38945365dc12a0906b690568b3c1ff1b2
Perarulalan V <95120766+perarulalan15@users.noreply.github.com>
2024-01-21 19:26:06 +0530

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('',include('horse_app.urls')),
+    path('',include('app.urls')),
     path('admin/', admin.site.urls),
 ]
